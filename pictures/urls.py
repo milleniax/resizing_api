dd86865e2c33fb17aa41bfc9ee0176bb6892fd19
milleniax <marsel.abdullin.00@mail.ru>
2020-06-12 19:45:26 +0300

    fix urls

--- pictures/urls.py
+++ pictures/urls.py
@@ -5,6 +5,7 @@ app_name = 'pictures'
 
 urlpatterns = [
     path('', views.pictures, name='pictures'),
+    path('rest/<int:pk>/', views.ContentView.as_view()),
     path('rest/', views.ContentView.as_view(), name='rest'),
     path('task/<str:task_id>/', views.TaskView.as_view(), name = 'task'),
 ]
